67b53ac14e1bc469911741ada862cb38e9583002
Marcelo Severgnini <marcelosevergnini@gmail.com>
2019-07-04 21:21:48 +0100

    Add app routes to router module

--- recipe-book-app/src/app/app-routing.module.ts
+++ recipe-book-app/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeHomeComponent } from './recipes/recipe-home/recipe-home.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent, 
+    children: [
+      { path: '', component: RecipeHomeComponent },
+      { path: ':id', component: RecipeDetailComponent}
+    ]
+  },
+  { path: 'shooping-list', component: ShoppingListComponent }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
